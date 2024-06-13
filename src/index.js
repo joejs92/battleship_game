6db143f221a9dc613b72847260832d3f642f4826
joejs92 <joejs_92@yahoo.com>
2024-06-13 14:48:11 -0400

    add Ship class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-function putOutSomething(text){
-    return text;
-}
+class Ship{
+    constructor(type,hitNumber){
+        this.type = type;
+        this.hitNumber = hitNumber;
+        this.sunk = false;
+        this.currentHits = 0;
+    }
 
-module.exports = putOutSomething;
+    isSunk(){
+        if(this.currentHits == this.hitNumber){
+            this.sunk = true;
+        }
+        return this.sunk;
+    }
+
+    hit(){
+        this.currentHits += 1;
+    }
+}
+//const playerCarrier = new Ship('carrier', 5);
+
+
+//module.exports = putOutSomething;
