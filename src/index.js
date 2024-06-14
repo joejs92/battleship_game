d9a1c0e7125e039bad25a0ab4069b9d264ba762f
joejs92 <joejs_92@yahoo.com>
2024-06-14 01:21:08 -0400

    add ship placement function

--- src/index.js
+++ src/index.js
@@ -49,44 +49,56 @@ class Gameboard{
         this.shipCoordinates = [[5,5]];
         this.misses = [];
         this.guesses = [];
+        this.carrier = new Ship('carrier',5);
+        this.battleship = new Ship('battleship',4);
+        this.destroyer = new Ship('destroyer',3);
+        this.submarine = new Ship('submarine',3);
+        this.ptBoat = new Ship('ptBoat',2);
+        this.ships = {
+            'carrier':this.carrier,
+            'battleship':this.battleship,
+            'destroyer':this.destroyer,
+            'submarine':this.destroyer,
+            'ptBoat':this.ptBoat
+        }
     }
 
     checkValidity(value,length,direction){
         //Checking that coordinates are on the board.
         if(value[0] > this.size || value[1] > this.size){
-            return "Unable to place";
+            return false;
         }
         //Checking that ship doesn't go off the board.
         if(direction == 'up'){
             if(value[1] + length -1 > this.size){
-                return "Unable to place";
+                return false;
             }
             else{
-                return "Fine";
+                return true;
             }
         }
         else if(direction == 'down'){
             if(value[1] - length +1 < 1){
-                return "Unable to place";
+                return false;
             }
             else{
-                return "Fine";
+                return true;
             } 
         }
         else if(direction == 'right'){
             if(value[0] + length -1 > this.size){
-                return "Unable to place";
+                return false;
             }
             else{
-                return "Fine";
+                return true;
             }
         }
         else if(direction == 'left'){
             if(value[0] - length +1 < 1){
-                return "Unable to place";
+                return false;
             }
             else{
-                return "Fine";
+                return true;
             } 
         }
     }
@@ -131,9 +143,46 @@ class Gameboard{
         }
         return collision;
     }
+
+    placeShip(value,length,direction){
+        if(this.checkValidity(value,length,direction) == true && this.checkCollision(value,length,direction) == false){
+            if(direction == 'up'){
+                for(let i = 0; i < length; i++){
+                    const coordinate = [value[0],value[1]+i];
+                    this.shipCoordinates.push(coordinate);
+                }
+                //return true;
+            }
+            else if(direction == 'down'){
+                for(let i = 0; i < length; i++){
+                    const coordinate = [value[0],value[1]-i];
+                    this.shipCoordinates.push(coordinate);
+                }
+                //return true;
+            }
+            else if(direction == 'right'){
+                for(let i = 0; i < length; i++){
+                    const coordinate = [value[0]+i,value[1]];
+                    this.shipCoordinates.push(coordinate);
+                }
+                //return true;
+            }
+            else if(direction == 'left'){
+                for(let i = 0; i < length; i++){
+                    const coordinate = [value[0]-i,value[1]];
+                    this.shipCoordinates.push(coordinate);
+                }
+                //return true;
+            }
+        }
+        else{
+            return false;
+        }
+    }
 }
 
 const gameboard = new Gameboard(10);
 //console.log(gameboard.checkValidity([10,10],5,'up'));
-//gameboard.checkCollision([5,1],5,'up');
+gameboard.placeShip([5,3],5,'up');
+console.log(gameboard.shipCoordinates);
 module.exports = gameboard;
--- src/index.test.js
+++ src/index.test.js
@@ -1,43 +1,44 @@
 const gameboard = require('./index');
 /*
 describe('Testing Validity', ()=> {
-
+    //Depends on a board size of 10
     it('Off the board; Both are over', () => {
-        expect(gameboard.checkValidity([11,13],5,'up')).toBe("Unable to place");
+        expect(gameboard.checkValidity([11,13],5,'up')).toBe(false);
     });
 
     it('Off the board; One is over', () => {
-        expect(gameboard.checkValidity([10,13],5,'up')).toBe("Unable to place");
+        expect(gameboard.checkValidity([10,13],5,'up')).toBe(false);
     });
     it('Off the board; Both are within', () => {
-        expect(gameboard.checkValidity([6,5],5,'up')).toBe("Fine");
+        expect(gameboard.checkValidity([6,5],5,'up')).toBe(true);
     });
     it('up,on', () => {
-        expect(gameboard.checkValidity([4,4],5,'up')).toBe("Fine");
+        expect(gameboard.checkValidity([4,4],5,'up')).toBe(true);
     });
     it('up,off', () => {
-        expect(gameboard.checkValidity([4,7],5,'up')).toBe("Unable to place");
+        expect(gameboard.checkValidity([4,7],5,'up')).toBe(false);
     });
     it('down,on', () => {
-        expect(gameboard.checkValidity([4,8],5,'down')).toBe("Fine");
+        expect(gameboard.checkValidity([4,8],5,'down')).toBe(true);
     });
     it('down,off', () => {
-        expect(gameboard.checkValidity([4,3],5,'down')).toBe("Unable to place");
+        expect(gameboard.checkValidity([4,3],5,'down')).toBe(false);
     });
     it('right,on', () => {
-        expect(gameboard.checkValidity([4,4],5,'right')).toBe("Fine");
+        expect(gameboard.checkValidity([4,4],5,'right')).toBe(true);
     });
     it('right,off', () => {
-        expect(gameboard.checkValidity([8,4],5,'right')).toBe("Unable to place");
+        expect(gameboard.checkValidity([8,4],5,'right')).toBe(false);
     });
     it('left,on', () => {
-        expect(gameboard.checkValidity([8,4],5,'left')).toBe("Fine");
+        expect(gameboard.checkValidity([8,4],5,'left')).toBe(true);
     });
     it('left,off', () => {
-        expect(gameboard.checkValidity([4,4],5,'left')).toBe("Unable to place");
+        expect(gameboard.checkValidity([4,4],5,'left')).toBe(false);
     });
 });
 describe('Testing Validity', ()=> {
+    //Depends on a given ship coordinate of [5,5].
     it('Collision Up', () => {
         expect(gameboard.checkCollision([5,1],5,'up')).toBe(true);
     });
@@ -52,5 +53,24 @@ describe('Testing Validity', ()=> {
     });
     it('No Collision', () => {
         expect(gameboard.checkCollision([3,3],5,'up')).toBe(false);
+    });
+});
+describe('Testing Ship Placement', ()=> {
+    //Depends on a given ship coordinate of [5,5].
+    //Still needs work to get correct.
+    it('Place Ship Up', () => {
+        expect(gameboard.placeShip([4,1],5,'up')).toBe(true);
+    });
+    it('Place Ship Down', () => {
+        expect(gameboard.placeShip([4,8],5,'down')).toBe(true);
+    });
+    it('Place Ship Right', () => {
+        expect(gameboard.placeShip([3,4],5,'right')).toBe(true);
+    });
+    it('Place Ship Left', () => {
+        expect(gameboard.placeShip([8,4],5,'left')).toBe(true);
+    });
+    it('Place Ship Collision', () => {
+        expect(gameboard.placeShip([5,3],5,'up')).toBe(false);
     });
 });*/
