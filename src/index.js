fbf78906473834831b2d5a4190e97593ee7aa44d
joejs92 <joejs_92@yahoo.com>
2024-06-14 00:18:34 -0400

    check valid placement and collision

--- src/index.js
+++ src/index.js
@@ -18,6 +18,122 @@ class Ship{
     }
 }
 //const playerCarrier = new Ship('carrier', 5);
+/* For the gameboard class I should be more attentive with
+creating good tests. First, what do we need?
+1. To generate a gameboard. Default size is 10 x 10.
+Just have the ship's coordinates, the misses, and the
+guesses as separate arrays.
+2. A function that places ships. Should have one input for
+where the bow of the ship will go, and another for which
+direction the stern will be pointing. Should also have
+which ship is being placed as an input.
+3. A function to check if ship placement is valid. It will
+make sure that no part of the ship will be off the board
+when placed, and that the ship being placed will not
+occupy the same spaces as a ship already placed.
+Should throw an error message or something if placement 
+is invalid.
+4. A receiveAttack function that takes a pair of 
+coordinates, determines whether the attack hit a ship, 
+then sends a hit to the right ship, or records the miss.
+Should take the coordinates as an input. First check if
+the coordinate has already been guessed. If not, see if
+it hits or misses. After that, send the coordinates to 
+the guess history array. 
+5. A function that reports on if all of the player's 
+ships have been sunk.
+*/
+class Gameboard{
+    constructor(size){
+        this.size = size;
+        this.shipCoordinates = [[5,5]];
+        this.misses = [];
+        this.guesses = [];
+    }
 
+    checkValidity(value,length,direction){
+        //Checking that coordinates are on the board.
+        if(value[0] > this.size || value[1] > this.size){
+            return "Unable to place";
+        }
+        //Checking that ship doesn't go off the board.
+        if(direction == 'up'){
+            if(value[1] + length -1 > this.size){
+                return "Unable to place";
+            }
+            else{
+                return "Fine";
+            }
+        }
+        else if(direction == 'down'){
+            if(value[1] - length +1 < 1){
+                return "Unable to place";
+            }
+            else{
+                return "Fine";
+            } 
+        }
+        else if(direction == 'right'){
+            if(value[0] + length -1 > this.size){
+                return "Unable to place";
+            }
+            else{
+                return "Fine";
+            }
+        }
+        else if(direction == 'left'){
+            if(value[0] - length +1 < 1){
+                return "Unable to place";
+            }
+            else{
+                return "Fine";
+            } 
+        }
+    }
 
-//module.exports = putOutSomething;
+    checkCollision(value,length,direction){
+        let collision = false;
+        if(direction == 'up'){
+            for(let i = 0; i < length; i++){
+                for(let j = 0; j < this.shipCoordinates.length; j++){
+                    if(value[0]== this.shipCoordinates[j][0] && value[1]+i == this.shipCoordinates[j][1]){
+                        collision = true;
+                    }
+                }
+            }
+        }
+        else if(direction == 'down'){
+            for(let i = 0; i < length; i++){
+                for(let j = 0; j < this.shipCoordinates.length; j++){
+                    if(value[0]== this.shipCoordinates[j][0] && value[1]-i == this.shipCoordinates[j][1]){
+                        collision = true;
+                    }
+                }
+            }
+        }
+        else if(direction == 'right'){
+            for(let i = 0; i < length; i++){
+                for(let j = 0; j < this.shipCoordinates.length; j++){
+                    if(value[0]+i == this.shipCoordinates[j][0] && value[1] == this.shipCoordinates[j][1]){
+                        collision = true;
+                    }
+                }
+            }
+        }
+        else if(direction == 'left'){
+            for(let i = 0; i < length; i++){
+                for(let j = 0; j < this.shipCoordinates.length; j++){
+                    if(value[0]-i == this.shipCoordinates[j][0] && value[1] == this.shipCoordinates[j][1]){
+                        collision = true;
+                    }
+                }
+            }
+        }
+        return collision;
+    }
+}
+
+const gameboard = new Gameboard(10);
+//console.log(gameboard.checkValidity([10,10],5,'up'));
+//gameboard.checkCollision([5,1],5,'up');
+module.exports = gameboard;
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,56 @@
-const putOutSomething = require('./index');
+const gameboard = require('./index');
+/*
+describe('Testing Validity', ()=> {
 
-it('Print Test', () => {
-    expect(putOutSomething("Hello, World!")).toBe("Hello, World!");
+    it('Off the board; Both are over', () => {
+        expect(gameboard.checkValidity([11,13],5,'up')).toBe("Unable to place");
+    });
+
+    it('Off the board; One is over', () => {
+        expect(gameboard.checkValidity([10,13],5,'up')).toBe("Unable to place");
+    });
+    it('Off the board; Both are within', () => {
+        expect(gameboard.checkValidity([6,5],5,'up')).toBe("Fine");
+    });
+    it('up,on', () => {
+        expect(gameboard.checkValidity([4,4],5,'up')).toBe("Fine");
+    });
+    it('up,off', () => {
+        expect(gameboard.checkValidity([4,7],5,'up')).toBe("Unable to place");
+    });
+    it('down,on', () => {
+        expect(gameboard.checkValidity([4,8],5,'down')).toBe("Fine");
+    });
+    it('down,off', () => {
+        expect(gameboard.checkValidity([4,3],5,'down')).toBe("Unable to place");
+    });
+    it('right,on', () => {
+        expect(gameboard.checkValidity([4,4],5,'right')).toBe("Fine");
+    });
+    it('right,off', () => {
+        expect(gameboard.checkValidity([8,4],5,'right')).toBe("Unable to place");
+    });
+    it('left,on', () => {
+        expect(gameboard.checkValidity([8,4],5,'left')).toBe("Fine");
+    });
+    it('left,off', () => {
+        expect(gameboard.checkValidity([4,4],5,'left')).toBe("Unable to place");
+    });
 });
+describe('Testing Validity', ()=> {
+    it('Collision Up', () => {
+        expect(gameboard.checkCollision([5,1],5,'up')).toBe(true);
+    });
+    it('Collision Down', () => {
+        expect(gameboard.checkCollision([5,8],5,'down')).toBe(true);
+    });
+    it('Collision Right', () => {
+        expect(gameboard.checkCollision([3,5],5,'right')).toBe(true);
+    });
+    it('Collision Left', () => {
+        expect(gameboard.checkCollision([8,5],5,'left')).toBe(true);
+    });
+    it('No Collision', () => {
+        expect(gameboard.checkCollision([3,3],5,'up')).toBe(false);
+    });
+});*/
